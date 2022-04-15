8d298a038658e282d0534ba4e02b36b8a41fdf94
Mdimase <dimasematias1@gmail.com>
2022-04-15 00:40:42 -0300

    validacion formulario login

--- my-music-front/src/app/components/login/login.component.ts
+++ my-music-front/src/app/components/login/login.component.ts
@@ -10,43 +10,50 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loading:boolean = false;
-  submitted:boolean = false;
-  loginForm!: FormGroup;
-  error:string='';
+  error:string='';  //mensaje de error ante un mal login
+  loginForm!: FormGroup;  
   constructor(private formBuilder: FormBuilder, 
     private route: ActivatedRoute,
     private router: Router,
-    private authenticationService: AuthenticationService) { }
+    private authenticationService: AuthenticationService){
+      this.loginForm = this.initForm();
+     }
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      email: ['',Validators.required],
-      password: ['',Validators.required]
-    });
     //reset login status
     this.authenticationService.logout();
   }
 
-  get f() { return this.loginForm.controls;}
+  initForm():FormGroup{
+    return this.formBuilder.group({
+      email: ['',[Validators.required,Validators.minLength(5),
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}'),],],
+      password: ['',[Validators.required,Validators.minLength(3)],]
+    });
+  }
+
+  // recibe un campo y determina si esta invalido o no
+  // touched sera true si se ingreso al campo input
+  // dirty sera true si se singreso al campo input y se ingreso algo
+  isInvalidField(field: string):boolean {
+    const fieldName = this.loginForm.get(field);
+    return fieldName!.invalid && (fieldName!.touched || fieldName!.dirty);
+  }
+
+  getMin(field:string):number{
+    return this.loginForm.get(field)?.errors?.['minlength'].requiredLength; 
+  }
 
   onSubmit() {
-    this.submitted = true;
-    //stop si el form es invalido
-    if (this.loginForm.invalid){
-      return;
-    }
-    this.loading = true;
-    //email y password ingresado por el usuario
-    this.authenticationService.login(this.loginForm.value.email, this.loginForm.value.password)
-          .pipe(first())
-          .subscribe(
-            (data: any) => {  //body del response
-              this.router.navigate(['playlists']);  //lleva al usuario a la vista de playlists
-            },
-            (error:any) => {
-              this.error = 'usuario incorrecto';
-              this.loading = false;
-            });
+    const email:String = this.loginForm.get('email')?.value;
+    const password:String = this.loginForm.get('password')?.value;
+    this.authenticationService.login(email,password)
+      .subscribe({
+        next:(data) => {  //body del response
+          this.router.navigate(['playlists']);  //lleva al usuario a la vista de playlists
+        },
+        error:(e) => {  // email/password incorrectos
+          this.error = 'email/password incorrectos';
+        }});
   }
 }
